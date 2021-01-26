d6487340d08b414b694720517c7ba03cf60eafc0
Steven Woodward <steveanthony999@gmail.com>
2021-01-26 14:47:49 -0800

    removed old node objects to make way for express

--- 04/index.js
+++ 04/index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
@@ -15,11 +15,14 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware');
-  // ...
+  // res.setHeader - you CAN override the express setHeader
+  res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 const PORT = 3000;
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
