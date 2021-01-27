87ba3340efb228e5e03d5b548f3915acffb1ea43
Steven Woodward <steveanthony999@gmail.com>
2021-01-26 20:42:53 -0800

    set up urlencoding

--- 06/index.js
+++ 06/index.js
@@ -1,7 +1,26 @@
-const app = require('express')();
+const express = require('express');
 
-app.use((req, res) => {
-  console.log('connect');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/favicon.ico', (req, res, next) => {
+  res.sendStatus(204);
+});
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express</h1>');
 });
 
 const PORT = 3000;
