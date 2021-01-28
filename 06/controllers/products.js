fcaebca422b99b5234050008a166dd81b669f27e
Steven Woodward <steveanthony999@gmail.com>
2021-01-28 00:17:36 -0800

    added models

--- 06/controllers/products.js
+++ 06/controllers/products.js
@@ -1,14 +1,18 @@
-const products = [];
+// const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', { docTitle: 'Add Product', path: '/admin/add-product' });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  //   products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', { prods: products, docTitle: 'Shop', path: '/shop' });
 };
